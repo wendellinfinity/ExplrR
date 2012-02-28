ddfce7ca2539a32df9793e7236c517124d96276e
Wendell <wendell.hack@gmail.com>
2012-02-28 19:12:15 +0800

    Initial commit

---
+++ ExplrR/Models/Explorer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExplrR.Models {
+
+    public class Coordinates {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class Explorer {
+
+        public string Name { get; set; }
+        public string StatusMessage { get; set; }
+        public Coordinates Location { get; set; }
+
+    }
+}
---
+++ ExplrR/World.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SignalR.Hubs;
+using ExplrR.Models;
+using System.Web.Script.Serialization;
+
+namespace ExplrR {
+    public class World : Hub {
+        
+        // 1. we changed our parameter now to "explorer"
+        public void Explore(string explorer) {
+            // 2. let us instantiate a JSON serializer to parse our JSON object
+            var serializer = new JavaScriptSerializer();
+            // 3. deserialize the JSON string, 'explorer' which was sent from the client
+            var explorerInstance = serializer.Deserialize<Explorer>(explorer);
+            // 4. let us manipulate some of the explorer instance properties, just an example
+            explorerInstance.Location.X += 1;
+            explorerInstance.Location.Y += 1;
+            // 5. then we can send the serialized object to JSON string again to the client 'updateExplorer' handler
+            Clients.updateExplorer(serializer.Serialize(explorerInstance));
+        }
+
+    }
+}
